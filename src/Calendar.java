8a7f52e96c9f71f8bd2debcd510e6238bf9238bf
lee jungvin <lee jungvin@192.168.123.107>
2020-07-31 13:56:25 +0900

    static&non-static

--- src/Calendar.java
+++ src/Calendar.java
@@ -4,20 +4,27 @@ public class Calendar {
 	
 	private static final int[] maxDate={31,28,31,30,31,30,31,31,30,31,30,31};
 	
-	public int maxDateMonth(int input) {
+	//static 메소드인 경우 
+	public static int maxDateMonth(int input) {
+		return maxDate[input-1];
+	}
+	//staic이 없는 경우
+	public int maxDateMonth2(int input) {
 		return maxDate[input-1];
 	}
 	
 	public static void main(String[] args) {
 		
 		Scanner scanner=new Scanner(System.in);
-		Calendar cal=new Calendar();
 		System.out.println("달을 입력하세요.");
 		int input=scanner.nextInt();
 		
-		System.out.printf("%d월은 %d일까지 있습니다.\n", input, cal.maxDateMonth(input)); //printf는 자동으로 줄바꿈이 되지 않음
-		//System.out.println(input+"월은 "+month[input-1]+"일까지 있습니다.");
-		scanner.close();
+		//static 메소드는 객체를 생성할 필요 없음
+		System.out.printf("%d월은 %d일까지 있습니다.\n", input, maxDateMonth(input)); //printf는 자동으로 줄바꿈이 되지 않음
+		//non static 메소드를 사용할 땐 객체를 생성해야함!
+		Calendar cal=new Calendar();
+		System.out.printf("%d월은 %d일까지 있습니다.\n", input, cal.maxDateMonth2(input)); 
 		
+		scanner.close();
 	}
 }
