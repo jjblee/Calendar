50e6ba9c3512d1e59e3e2c83dea36182445084d0
lee jungvin <lee jungvin@192.168.123.107>
2020-08-01 15:29:38 +0900

    달력 찍기

---
+++ src/Prompt.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Prompt {
+	
+	private static final String PROMPT="cal> ";
+	private static final int[] MaxDATE={31,28,31,30,31,30,31,31,30,31,30,31};
+	
+	public static int MaxDateMonth(int month) {
+		return MaxDATE[month-1];
+	}
+	
+	public static void runPrompt() {
+		Scanner scanner=new Scanner(System.in);
+		int month=1; //while문 밖에서도 사용하기 위함!
+		while(true) {
+			System.out.println("월을 입력하세요.");
+			System.out.print(PROMPT); //줄바꿈 없음
+			month=scanner.nextInt();
+			if(month==-1) {
+				break;
+			} else if(month>12) {
+				continue;
+			}
+			printCalendar(2020, month);
+		}
+		System.out.println("종료");
+		scanner.close();
+	}
+	
+	public static void printCalendar(int year, int month) {
+		System.out.printf("     <%4d년 %3d월>\n", year, month);
+		System.out.println(" 일   월    화    수   목    금   토");
+		System.out.println("--------------------");
+
+		int maxDate=MaxDateMonth(month);
+		for(int i=1; i<=maxDate; i++) {
+			System.out.printf("%3d",i);
+			if(i%7==0) {
+				System.out.printf("\n");
+			}
+		}
+	}
+		
+	
+	public static void main(String[] args) {
+		runPrompt();
+	}
+}
